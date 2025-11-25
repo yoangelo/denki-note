8c5b51b2da0cd0dcab07de31859b155514afc1ee
yoangelo <edamame24051523@gmail.com>
2025-11-26 06:25:05 +0900

    fix: CORS設定の環境変数名を修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CORS_ALLOWED_ORIGINS", "").split(",")
+    origins ENV.fetch("CORS_ORIGINS", "").split(",")
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
